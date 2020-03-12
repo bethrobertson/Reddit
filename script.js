a6960ab1726bc60408464ea691641a57f09e028e
Beth Robertson <beth@stripe.com>
2020-03-11 20:31:54 -0400

    add new files

---
+++ script.js
@@ -0,0 +1,28 @@
+// Note the new "async" and "await" keywords.
+window.onload = async function() {
+
+    // Fetch is built in, just like window and document.
+    // await indicates that we have to wait for the request
+    // to finish. 
+    let r = await fetch('https://www.reddit.com/r/all/.json');
+    
+    // Processing this response into a an Object from the
+    // JSON response is also asynchronous
+    let responseJSON = await r.json();
+  
+    // Now that we have the data and it's in a format
+    // that we can easily use, we'll create links of
+    // our own. 
+  
+    for(post of responseJSON.data.children) {
+      let header = document.createElement('h3');
+      let anchor = document.createElement('a');
+      
+      anchor.href = post.data.url;
+      anchor.innerText = post.data.title;
+  
+      header.appendChild(anchor);
+      document.body.appendChild(header);
+    }
+  }
+  
